d1e9981ba3403a389257572aa10580f86069fbfd
manas-droid <manas.kalangan@gmail.com>
2026-02-08 17:49:18 -0500

    Morning brief works

--- src/ai/gemini.model.ts
+++ src/ai/gemini.model.ts
@@ -1,13 +1,20 @@
-import {GenerativeModel, GoogleGenerativeAI} from '@google/generative-ai'
+import {GenerativeModel, GoogleGenerativeAI, HarmBlockThreshold, HarmCategory} from '@google/generative-ai'
+import { Tool } from '@google/generative-ai/server';
 
 
 
-export function getGeminiModel(model_name:string = 'gemini-2.5-flash'):GenerativeModel{
+export function getGeminiModel(model_name:string = 'gemini-2.5-flash', ):GenerativeModel{
     const ai = new GoogleGenerativeAI('AIzaSyC0TeX6AN7szRr8Y3hl8_j_OybrLH4TZNg');
 
     const model = ai.getGenerativeModel({
         model: model_name,
-        generationConfig: { responseMimeType: "application/json" }
+        generationConfig: { responseMimeType: "application/json" },
+        safetySettings: [
+            {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,            
+            }
+        ]
     })
 
     return model;
